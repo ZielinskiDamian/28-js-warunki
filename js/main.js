8b449907ed04fa1f0f958a17c56565ea6b61bd02
damian zielinski <dazielinscy@gmail.com>
2024-05-16 08:30:26 +0200

    zmiany

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let num = 10;
+if (num > 10) {
+	console.log('Podana liczna jest wieksza od 10');
+} else if (num < 10) {
+	console.log('Podana liczba jest mnijesz niż 10');
+} else if (num == 10) {
+	console.log('podana liczna jest równa 10');
+}
